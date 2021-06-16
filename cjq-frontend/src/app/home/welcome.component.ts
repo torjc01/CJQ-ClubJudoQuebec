e1b9ead197ac187b59117673e5206d64f3e4409d
torjc01 <julio_cesar.torres@sct.gouv.qc.ca>
2021-06-16 10:10:22 -0400

    Update welcome component
    Signed-off-by: torjc01 <julio_cesar.torres@sct.gouv.qc.ca>

--- cjq-frontend/src/app/home/welcome.component.ts
+++ cjq-frontend/src/app/home/welcome.component.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
+import { ContactService } from '../shared/contact.service';
 
 @Component({
   templateUrl: './welcome.component.html'
 })
 export class WelcomeComponent {
-  public pageTitle = 'Welcome to the Club de Judo de Quebec';
+  public pageTitle = 'Bienvenu au Club de Judo de Québec';
+  public vale = 1;
+  
+
+
+  onClick(): void {
+    console.log('Clicou no botao de envio');
+    let res = this.validateInput();
+    console.log("Validation: ", res);
+    if(res){
+      let res = this.submitContact();
+      console.log("Submit: ", res); 
+    }
+  }
+
+  validateInput(): boolean {
+    if(this.vale == 1 ){
+      return true;
+    }
+    return false;
+  }
+
+  submitContact(): boolean{
+    //this.ContactService.
+    return false;
+  }
 }
