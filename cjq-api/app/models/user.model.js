cb34eb44c7524bf0b67cc891fa60aeb89772473d
torjc01 <julio_cesar.torres@sct.gouv.qc.ca>
2021-06-21 01:09:39 -0400

    Add user endpoint
    Signed-off-by: torjc01 <julio_cesar.torres@sct.gouv.qc.ca>

--- cjq-api/app/models/user.model.js
+++ cjq-api/app/models/user.model.js
@@ -1,7 +1,24 @@
-const sql = require('.db');
+const sql = require('./db');
 
 const User = function(user){
+    this.username           = user.username; 
+    this.codeMembre         = user.codeMembre; 
+    this.motPasse           = user.motPasse; 
+    this.statusUser         = user.statusUser; 
+    this.niveauPermission   = user.niveauPermission;
+};
 
+User.create = (newUser, result) => {
+    sql.query("INSERT INTO USER SET ?", newUser, (err, res) =>{
+        if(err){
+            console.log("error: ", err); 
+            result(err, null); 
+            return; 
+        }
+
+        console.log("User created: ", { id: res.insertId, ...newUser });
+        result(null, {id: res.insertId, ...newUser}); 
+    });
 };
 
 module.exports = User;
--- cjq-api/app/routes/user.routes.js
+++ cjq-api/app/routes/user.routes.js
@@ -2,8 +2,18 @@ module.exports = app => {
 
     const users = require('../controllers/user.controller.js'); 
 
-    app.post('/users', );
+    // Creation d'un nouveau user
+    app.post('/users', users.create);
 
-    app.get('/users/:codeUser'); 
+    // Lire tous les users
+    app.get('/users', users.findAll); 
 
+    // Lire un user spécifique
+    app.get('/users/:codeUser', users.findOne); 
+
+    // Mettre a jour un user 
+    app.patch('/users/:codeUser', users.update); 
+
+    // Supprime un user specifique
+    app.delete('/users/:codeUser', users.delete);
 }
