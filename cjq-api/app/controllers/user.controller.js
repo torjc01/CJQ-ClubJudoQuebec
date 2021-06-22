6895e2b531dba0ed757c19970be9ae6a086fc6d9
torjc01 <julio_cesar.torres@sct.gouv.qc.ca>
2021-06-22 14:52:16 -0400

    Add findById, update and delete to endpoint
    Signed-off-by: torjc01 <julio_cesar.torres@sct.gouv.qc.ca>

--- cjq-api/app/controllers/user.controller.js
+++ cjq-api/app/controllers/user.controller.js
@@ -42,10 +42,65 @@ exports.findAll = (req, res) => {
 };
 
 // Find a single user with a codeUser
-exports.findOne = (req, res) => {};
+exports.findOne = (req, res) => {
+    User.findById(req.params.codeUser, (err, data)=>{
+        if (err) {
+            if (err.kind === "not_found") {
+              res.status(404).send({
+                message: `Not found user with id ${req.params.codeUser}.`
+              });
+            } else {
+              res.status(500).send({
+                message: "Error retrieving user with id " + req.params.codeUser
+              });
+            }
+        } else res.send(data);
+    })
+};
 
 // Update a user identifyied by codeUser in the request
-exports.update = (req, res) => {}; 
+exports.update = (req, res) => {
+    // Validate Request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  }
+
+  console.log(req.body);
+
+  User.updateById(
+    req.params.codeUser,
+    new User(req.body),
+    (err, data) => {
+      if (err) {
+        if (err.kind === "not_found") {
+          res.status(404).send({
+            message: `Not found user with id ${req.params.codeUser}.`
+          });
+        } else {
+          res.status(500).send({
+            message: "Error updating user with id " + req.params.codeUser
+          });
+        }
+      } else res.send(data);
+    }
+  );
+}; 
 
 // Delete a user with a specified codeUser
-exports.delete = (req, res) => {}; 
+exports.delete = (req, res) => {
+    User.remove(req.params.codeUser, (err, data) => {
+        if (err) {
+          if (err.kind === "not_found") {
+            res.status(404).send({
+              message: `Not found user with id ${req.params.codeUser}.`
+            });
+          } else {
+            res.status(500).send({
+              message: "Could not delete user with id " + req.params.codeUser
+            });
+          }
+        } else res.send({ message: `User was deleted successfully!` });
+      });
+}; 
--- cjq-api/app/models/user.model.js
+++ cjq-api/app/models/user.model.js
@@ -33,4 +33,72 @@ User.getAll = result => {
     });
 }; 
 
+User.findById = (codeUser, result) => {
+    sql.query("SELECT * FROM USER WHERE codeUser = ?", codeUser, (err, res) =>{
+        if(err){
+            console.log("Error: ", err);
+            result(err, null);
+            return;
+        }
+
+        if (res.length) {
+            console.log("found user: ", res[0]);
+            result(null, res[0]);
+            return;
+        }
+
+        // not found user with the id
+        result({ kind: "not_found" }, null);
+    });
+};
+
+User.updateById = (codeUser, user, result) => {
+    sql.query("UPDATE USER SET username=?, codeMembre=?, motPasse=?, statusUser=?, niveauPermission=? WHERE codeUser=?",
+        [
+         user.username, 
+         user.codeMembre, 
+         user.motPasse, 
+         user.statusUser, 
+         user.niveauPermission,
+         codeUser
+        ],
+        (err, res) => {
+            if (err) {
+                console.log("Error: ", err);
+                result(null, err);
+                return;
+            }
+    
+            if (res.affectedRows == 0) {
+            // not found contact with the id
+            result({ kind: "not_found" }, null);
+            return;
+            }
+    
+            console.log("updated user: ", { codeUser: codeUser, ...user });
+            result(null, { codeUser: codeUser, ...user });
+        }
+    ); 
+
+};
+
+User.remove = (codeUser, result) => {
+    sql.query("DELETE FROM USER WHERE codeUser = ?", codeUser, (err, res) => {
+        if (err) {
+          console.log("Error: ", err);
+          result(null, err);
+          return;
+        }
+    
+        if (res.affectedRows == 0) {
+          // not found contact with the codeContact
+          result({ kind: "not_found" }, null);
+          return;
+        }
+    
+        console.log("deleted user with codeContact: ", codeUser);
+        result(null, res);
+      });
+}
+
 module.exports = User;
